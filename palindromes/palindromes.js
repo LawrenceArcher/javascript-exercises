67b9d026ad1c852ec7094c6779019d04b8c8d3a2
Lawrence Archer <lawrence.archer@gmail.com>
2018-02-07 21:35:07 +0000

    finish palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-var palindromes = function() {
-
+var palindromes = function(str) {
+	var clean = str.replace(/\W/g, '').toLowerCase();
+	console.log(clean);
+	var arr = clean.split("");
+	var reverseArr = [];
+	var j = 0;
+	for (var i = arr.length-1; i >= 0; i--){
+		reverseArr[j]=arr[i];
+		j++;
+	}
+	var reverseStr = reverseArr.join("");
+	return reverseStr == clean? true:false;
 }
 
 module.exports = palindromes
