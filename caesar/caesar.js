c4c0810ffd55e8077836ff2d066703fd44d459b7
Lawrence Archer <lawrence.archer@gmail.com>
2018-02-09 06:24:29 +0000

    start caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,15 @@
-var caesar = function() {
+var caesar = function(str, num) {
+	var arr = [];
 
+	for (var i = 0; i<str.length;i++){
+		if (str[i] == "," || str[i]=="." || str[i]=="?"){
+			arr.push(str[i].charCodeAt());
+		}else{
+			arr.push(str[i].charCodeAt() + num);
+		}
+	}
+	console.log(arr);
+	return String.fromCharCode(...arr);
 }
 
 module.exports = caesar
