ad113a3ac37da734b298fc1221a6440087d889f4
Lawrence Archer <lawrence.archer@gmail.com>
2018-02-10 19:51:38 +0000

    finish fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-var fibonacci = function() {
-
+var fibonacci = function(num) {
+	if (num<0){
+		return "OOPS";
+	}
+	var arr = [0, 0];
+	var nextNum = 0;
+	for (var i = 0; i<num;i++){
+		if (i ==0){
+			nextNum = 1;
+		}else{
+			nextNum = arr[0] + arr[1];
+		}
+		arr.shift();
+		arr.push(nextNum);
+	}
+	return arr[1];
 }
 
 module.exports = fibonacci
+//shift first element then push new element.
+//return 3rd element of array
