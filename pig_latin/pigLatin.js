f77f6ae254f37bb353c2b762a0bf88455a94c493
Lawrence Archer <lawrence.archer@gmail.com>
2018-03-06 07:47:56 +0000

    start pigLatin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,18 @@
-function translate() {
-	// body...
+function translate(input) {
+	let ay = "ay";
+	let output = "";
+	if (isVowel(input[0])){
+		output = input.concat(ay);
+	}
+	return output;
 }
-
+function isVowel(char) {
+  if (char.length == 1)
+  {
+    return /[aeiou]/.test(char);
+  }
+}
 
 module.exports = {
 	translate
 }
-
